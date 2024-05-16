2e1b1204d9400f320f4d0ef1c94bb3a877e005ff
kkwoong <kkwkim0528@naver.com>
2024-05-16 19:03:41 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // CORS 미들웨어 추가
 app.use(cors());
